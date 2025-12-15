09bf4c909fbe1fcfed9615d5b6818fcb72ea5abd
Adam Beres <info@adamberes.de>
2025-12-15 21:07:17 +0100

    changed

--- my-app/src/assets/scripts/app.js
+++ my-app/src/assets/scripts/app.js
@@ -5,7 +5,7 @@ import { apiKey } from "./util.js";
 // import * as util from "./util.js";
 console.log("util.apiKey");
 
-console.log(util.apiKey);
+console.log(apiKey);
 // console.log(content);
 
 // const userMessage = "Hello World!!!";
