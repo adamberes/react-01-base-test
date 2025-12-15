57a681c349d19b0e07eed7895d52ae053e474c8d
adam beres <adamberes60@gmail.com>
2025-12-15 16:23:47 +0000

    chanfed added a line

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -5,6 +5,7 @@ import { apiKey } from "./util.js";
 // import * as util from "./util.js";
 console.log("util.apiKey1");
 console.log("util.apiKey2");
+console.log("util.apiKey3");
 
 console.log(apiKey);
 // console.log(content);
