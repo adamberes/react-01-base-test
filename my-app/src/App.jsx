ef478e40e335ffb76a70cdbc6b4585df24fa7242
Adam Beres <info@adamberes.de>
2025-12-18 18:15:03 +0100

    Update header text in App component2

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React 66ad</h1>
+      <h1>Vite + React 66ade</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}
          className="button"
